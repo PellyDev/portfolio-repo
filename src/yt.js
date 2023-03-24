cff6119d740bff8fc2d13fed40e341988cbb626f
Pellyyy <zabookinkajou@protonmail.com>
2023-03-24 16:02:14 +0100

    implemented yt slider (WIP)

--- src/yt.js
+++ src/yt.js
@@ -4,24 +4,33 @@ const API_BASE_URL =
     "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId=UUi7SpBeS4FXazpYJOmxGAeg&key="
 const API_KEY = "AIzaSyBOMHDByZ065Tq8OUJSL55FOQEqZz2vQZ8"
 
+// amount of yt vids that get returnd from the api call
+let length = 0
+
 export function getVids() {
     fetch(API_BASE_URL + API_KEY)
         .then((res) => res.json())
-        .then((data) => generateVids(data))
+        .then((data) => {
+            generateVids(data)
+            length = data.items.length
+            console.log(length)
+        })
 }
 
 function generateVids(json) {
-    const parent = document.querySelector(".yt-container")
-    const template = document.querySelector(".yt-container template")
+    const parent = document.querySelector(".slider")
+    const template = document.querySelector(".slider template")
     json.items.forEach((vid) => {
         const { title, description, publishedAt: date } = vid.snippet
         const { url: thumnbnail } = vid.snippet.thumbnails.maxres
         const { videoId } = vid.snippet.resourceId
 
         const el = template.content.cloneNode(true)
-        el.querySelector("h3").innerText = title
         el.querySelector("img").src = thumnbnail
         el.querySelector("a").href = WATCH_BASE_URL + videoId
         parent.appendChild(el)
     })
 }
+
+const leftArrow = document.querySelector(".arrow.left")
+const rightArrow = document.querySelector(".arrow.right")
