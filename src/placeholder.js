f73dff6f1342026e0ebb704932fb2a5492e92360
Pellyyy <116307480+Pellyyy@users.noreply.github.com>
2023-03-25 07:46:52 +0100

    slider scroll + fixed scroll behavior

---
+++ src/placeholder.js
@@ -0,0 +1,255 @@
+export const placeholder = {
+  kind: "youtube#playlistItemListResponse",
+  etag: "n_Jh22LZ0_97K8uQj2k0GCmxiYE",
+  items: [
+    {
+      kind: "youtube#playlistItem",
+      etag: "R3mu5ESNhURq6SsC0f6sI1-jn3k",
+      id: "VVVpN1NwQmVTNEZYYXpwWUpPbXhHQWVnLkJ1M3FpTnV6RUo4",
+      snippet: {
+        publishedAt: "2023-03-22T13:18:04Z",
+        channelId: "UCi7SpBeS4FXazpYJOmxGAeg",
+        title:
+          'In-depth Explanation of DFS Approach! | Leetcode 2492 - "Minimum Score of a Path Between Two Cities"',
+        description:
+          "We're using DFS to solve leetcode problem 2492- \"Minimum Score of a Path Between Two Cities\".\nLet me know if you'd like to see a video on how to solve this problem using doubly linked lists.\nIf this helped you at all and you enjoyed my video, please consider liking and subscribing! :)\n\n00:00 Explaining the problem\n02:01 In-depth explanation of Adjacency Matrix and DFS Approach\n07:05 Coding the solution \n\n\nCheck out the solution on Leetcode: https://leetcode.com/problems/minimum-score-of-a-path-between-two-cities/solutions/3328798/video-explanation-easy-dfs-solution-in-javascript/",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/Bu3qiNuzEJ8/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/Bu3qiNuzEJ8/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/Bu3qiNuzEJ8/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+          standard: {
+            url: "https://i.ytimg.com/vi/Bu3qiNuzEJ8/sddefault.jpg",
+            width: 640,
+            height: 480,
+          },
+          maxres: {
+            url: "https://i.ytimg.com/vi/Bu3qiNuzEJ8/maxresdefault.jpg",
+            width: 1280,
+            height: 720,
+          },
+        },
+        channelTitle: "Pelly",
+        playlistId: "UUi7SpBeS4FXazpYJOmxGAeg",
+        position: 0,
+        resourceId: {
+          kind: "youtube#video",
+          videoId: "Bu3qiNuzEJ8",
+        },
+        videoOwnerChannelTitle: "Pelly",
+        videoOwnerChannelId: "UCi7SpBeS4FXazpYJOmxGAeg",
+      },
+    },
+    {
+      kind: "youtube#playlistItem",
+      etag: "zIuyQdEll8cjR-txfSs32hf6byY",
+      id: "VVVpN1NwQmVTNEZYYXpwWUpPbXhHQWVnLnQtNlZoWW8tb1dN",
+      snippet: {
+        publishedAt: "2023-03-21T17:39:51Z",
+        channelId: "UCi7SpBeS4FXazpYJOmxGAeg",
+        title:
+          'Easy solution in JS | Leetcode 2348 - "Number of Zero-Filled Subarrays"',
+        description:
+          "Sorry for the crackling, i'll try to fix it in the next video. :( \nIf this helped you at all and you enjoyed my video, please consider liking and subscribing! :)\n\n00:00 Explaining the problem\n00:56 Explaining the algorithm \n4:10 Coding the solution\n\nCheck out the solution on Leetcode: https://leetcode.com/problems/number-of-zero-filled-subarrays/solutions/3325806/video-explanation-easy-js-solution/\n\n#leetcode #leetcodedailychallenge #javascript",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/t-6VhYo-oWM/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/t-6VhYo-oWM/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/t-6VhYo-oWM/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+          standard: {
+            url: "https://i.ytimg.com/vi/t-6VhYo-oWM/sddefault.jpg",
+            width: 640,
+            height: 480,
+          },
+          maxres: {
+            url: "https://i.ytimg.com/vi/t-6VhYo-oWM/maxresdefault.jpg",
+            width: 1280,
+            height: 720,
+          },
+        },
+        channelTitle: "Pelly",
+        playlistId: "UUi7SpBeS4FXazpYJOmxGAeg",
+        position: 1,
+        resourceId: {
+          kind: "youtube#video",
+          videoId: "t-6VhYo-oWM",
+        },
+        videoOwnerChannelTitle: "Pelly",
+        videoOwnerChannelId: "UCi7SpBeS4FXazpYJOmxGAeg",
+      },
+    },
+    {
+      kind: "youtube#playlistItem",
+      etag: "yf-y4W-oIdBpZLWNv8nPKvEyAJw",
+      id: "VVVpN1NwQmVTNEZYYXpwWUpPbXhHQWVnLi1kTWR6aXVrd0xB",
+      snippet: {
+        publishedAt: "2023-03-18T12:57:23Z",
+        channelId: "UCi7SpBeS4FXazpYJOmxGAeg",
+        title:
+          'SOLUTION USING STACKS in JavaScript | Leetcode 1472 - "Design Browser History"',
+        description:
+          "We're using stacks to solve leetcode problem 958 - \"Design Browser History\".\nLet me know if you'd like to see a video on how to solve this problem using doubly linked lists.\nIf this helped you at all and you enjoyed my video, please consider liking and subscribing! :)\n\nCheck out the solution on Leetcode: \n\n00:00 Explaining the problem\n00:48 Explaining the algorithm \n04:40 Coding the solution",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/-dMdziukwLA/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/-dMdziukwLA/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/-dMdziukwLA/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+          standard: {
+            url: "https://i.ytimg.com/vi/-dMdziukwLA/sddefault.jpg",
+            width: 640,
+            height: 480,
+          },
+          maxres: {
+            url: "https://i.ytimg.com/vi/-dMdziukwLA/maxresdefault.jpg",
+            width: 1280,
+            height: 720,
+          },
+        },
+        channelTitle: "Pelly",
+        playlistId: "UUi7SpBeS4FXazpYJOmxGAeg",
+        position: 2,
+        resourceId: {
+          kind: "youtube#video",
+          videoId: "-dMdziukwLA",
+        },
+        videoOwnerChannelTitle: "Pelly",
+        videoOwnerChannelId: "UCi7SpBeS4FXazpYJOmxGAeg",
+      },
+    },
+    {
+      kind: "youtube#playlistItem",
+      etag: "qOjkNCFLCJMt132xWfITg8oJP_k",
+      id: "VVVpN1NwQmVTNEZYYXpwWUpPbXhHQWVnLnZfSlNnWE1Rc3Q0",
+      snippet: {
+        publishedAt: "2023-03-15T12:26:01Z",
+        channelId: "UCi7SpBeS4FXazpYJOmxGAeg",
+        title:
+          'EASY & INTUITIVE BFS SOLUTION | Leetcode 958 - "Check Completeness of a Binary Tree"',
+        description:
+          'A BFS solution for the leetcode problem 958 - "Check Completeness of a Binary Tree".\nIf this helped you at all and you enjoyed my video, please consider liking and subscribing! :)\n\nCheck out the solution on Leetcode: https://leetcode.com/problems/check-completeness-of-a-binary-tree/solutions/3300147/video-explanation-easy-bfs-solution-for-js/\n\n00:00 Explaining the problem\n02:30 Explaining the algorithm\n05:21 Time & Space Complexity\n06:22 Coding the solution\n\n#tree  #binarytree #bfs #leetcode #leetcodedailychallenge #breadthfirstsearch',
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/v_JSgXMQst4/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/v_JSgXMQst4/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/v_JSgXMQst4/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+          standard: {
+            url: "https://i.ytimg.com/vi/v_JSgXMQst4/sddefault.jpg",
+            width: 640,
+            height: 480,
+          },
+          maxres: {
+            url: "https://i.ytimg.com/vi/v_JSgXMQst4/maxresdefault.jpg",
+            width: 1280,
+            height: 720,
+          },
+        },
+        channelTitle: "Pelly",
+        playlistId: "UUi7SpBeS4FXazpYJOmxGAeg",
+        position: 3,
+        resourceId: {
+          kind: "youtube#video",
+          videoId: "v_JSgXMQst4",
+        },
+        videoOwnerChannelTitle: "Pelly",
+        videoOwnerChannelId: "UCi7SpBeS4FXazpYJOmxGAeg",
+      },
+    },
+    {
+      kind: "youtube#playlistItem",
+      etag: "sNe9pByu6QaBG2ge_hkgEF1y4Ag",
+      id: "VVVpN1NwQmVTNEZYYXpwWUpPbXhHQWVnLlZzaUZETUpaVnBF",
+      snippet: {
+        publishedAt: "2023-03-14T13:18:14Z",
+        channelId: "UCi7SpBeS4FXazpYJOmxGAeg",
+        title:
+          'Recursive DFS Solution in JavaScript! | Leetcode Problem 129 - "Sum Root to Leaf Numbers"',
+        description:
+          "A recursive DFS solution for the leetcode problem 129 - Sum Root to Leaf Numbers.\nIf this helped you at all and you enjoyed my video, please consider liking and subscribing! :)\n\nCheck out the solution on Leetcode: https://leetcode.com/problems/sum-root-to-leaf-numbers/solutions/3295177/simple-dfs-solution-beats-100-time/?orderBy=most_votes\n\n#leetcode #leetcodedailychallenge #javascript #typescript #recursion #depthfirstsearch #dfs #129 #binarytree #preorder #treetraversal #trees #nodes",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/VsiFDMJZVpE/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/VsiFDMJZVpE/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/VsiFDMJZVpE/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+          standard: {
+            url: "https://i.ytimg.com/vi/VsiFDMJZVpE/sddefault.jpg",
+            width: 640,
+            height: 480,
+          },
+          maxres: {
+            url: "https://i.ytimg.com/vi/VsiFDMJZVpE/maxresdefault.jpg",
+            width: 1280,
+            height: 720,
+          },
+        },
+        channelTitle: "Pelly",
+        playlistId: "UUi7SpBeS4FXazpYJOmxGAeg",
+        position: 4,
+        resourceId: {
+          kind: "youtube#video",
+          videoId: "VsiFDMJZVpE",
+        },
+        videoOwnerChannelTitle: "Pelly",
+        videoOwnerChannelId: "UCi7SpBeS4FXazpYJOmxGAeg",
+      },
+    },
+  ],
+  pageInfo: {
+    totalResults: 5,
+    resultsPerPage: 25,
+  },
+}
--- src/yt.js
+++ src/yt.js
@@ -1,36 +1,63 @@
 // API key has been restricted so it doesn't need to be obfuscated or stored in a .env file
 const WATCH_BASE_URL = "https://www.youtube.com/watch?v="
 const API_BASE_URL =
-    "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId=UUi7SpBeS4FXazpYJOmxGAeg&key="
+  "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId=UUi7SpBeS4FXazpYJOmxGAeg&key="
 const API_KEY = "AIzaSyBOMHDByZ065Tq8OUJSL55FOQEqZz2vQZ8"
 
 // amount of yt vids that get returnd from the api call
 let length = 0
 
 export function getVids() {
-    fetch(API_BASE_URL + API_KEY)
-        .then((res) => res.json())
-        .then((data) => {
-            generateVids(data)
-            length = data.items.length
-            console.log(length)
-        })
-}
-
-function generateVids(json) {
-    const parent = document.querySelector(".slider")
-    const template = document.querySelector(".slider template")
-    json.items.forEach((vid) => {
-        const { title, description, publishedAt: date } = vid.snippet
-        const { url: thumnbnail } = vid.snippet.thumbnails.maxres
-        const { videoId } = vid.snippet.resourceId
-
-        const el = template.content.cloneNode(true)
-        el.querySelector("img").src = thumnbnail
-        el.querySelector("a").href = WATCH_BASE_URL + videoId
-        parent.appendChild(el)
+  fetch(API_BASE_URL + API_KEY)
+    .then((res) => res.json())
+    .then((data) => {
+      generateVids(data)
+      length = data.items.length
     })
 }
 
+export function generateVids(json) {
+  const parent = document.querySelector(".slider")
+  const template = document.querySelector(".slider template")
+  json.items.forEach((vid) => {
+    const { title, description, publishedAt: date } = vid.snippet
+    const { url: thumnbnail } = vid.snippet.thumbnails.maxres
+    const { videoId } = vid.snippet.resourceId
+
+    const el = template.content.cloneNode(true)
+    el.querySelector("img").src = thumnbnail
+    el.querySelector("a").href = WATCH_BASE_URL + videoId
+    parent.appendChild(el)
+  })
+}
+
+const slider = document.querySelector(".slider")
 const leftArrow = document.querySelector(".arrow.left")
 const rightArrow = document.querySelector(".arrow.right")
+
+leftArrow.addEventListener("click", () => {
+  const style = getComputedStyle(slider)
+  let scrollIdx = Number(style.getPropertyValue("--scroll-index"))
+  if (scrollIdx === 0) {
+    console.log("hiii")
+    scrollIdx = (length - 2) / 2
+  } else {
+    // ceil in case we have a .5 max scroll idx
+    scrollIdx = Math.ceil(scrollIdx - 1)
+  }
+  slider.style.setProperty("--scroll-index", `${scrollIdx}`)
+  console.log(scrollIdx)
+})
+
+rightArrow.addEventListener("click", () => {
+  const style = getComputedStyle(slider)
+  let scrollIdx = Number(style.getPropertyValue("--scroll-index"))
+  if (scrollIdx < (length - 2) / 2) {
+    ++scrollIdx
+    scrollIdx = scrollIdx > (length - 2) / 2 ? (length - 2) / 2 : scrollIdx
+  } else {
+    scrollIdx = 0
+  }
+  slider.style.setProperty("--scroll-index", `${scrollIdx}`)
+  console.log(scrollIdx)
+})
